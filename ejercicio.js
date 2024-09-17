fa45f16d06ada20d46564768c203d76d377bf8d4
VaninaBlas <vanyabrilconblas@gmail.com>
2024-09-17 13:55:53 -0300

    add botones

--- ejercicio.js
+++ ejercicio.js
@@ -4,6 +4,7 @@ let container= document.querySelector(".container")
 
 class Item{
     constructor(valor ){
+        this.valor=valor
         this.crearDiv(valor)
     }
     crearDiv(valor){
@@ -13,7 +14,16 @@ class Item{
         inputitem.classList.add("item-input")
         inputitem.appendChild(valor)
         let div= document.createElement("id")
-        div.appendChild(inputitem)
         div.classList.add("item")
+        let botonEditar= document.createElement("button")
+        botonEditar.innerHTML= "<i class='fa-solid fa-lock'></i>"
+        botonEditar.classList.add("boton-editar")
+        let botonRemover= document.createElement("button")
+        botonRemover.innerHTML="<i class='fa-solid fa-trash'></i>"
+        botonRemover.classList.add("boton-remover")
+        div.appendChild(inputitem)
+        div.appendChild(botonEditar)
+        div.appendChild(botonRemover)
+        container.appendChild(div)
     }
 }
